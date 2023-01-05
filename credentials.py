3531038cdd9aa826d565101b869d551bcfda91b3
Mykhailo Ziabrov <zyabrov@gmail.com>
2023-01-05 23:17:26 +0200

    new adminbot connectected

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
-bot_token = "5700135657:AAGRBRImzcM0w0TD3PXQUug6y6CzofGM74w"
-bot_user_name = "loyaltybots_bot"
+bot_token = "5809303804:AAFlJTthvlh0_HFu9ZRAv7g_yQ69eU9tMO8"
+bot_user_name = "loyalty_adminbot"
 webhook_url = f"https://ziabrov.pythonanywhere.com/{bot_token}"
