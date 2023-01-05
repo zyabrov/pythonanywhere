d4ff7b75bae0a9b7900c00e09174cad8555a7186
Mykhailo Ziabrov <zyabrov@gmail.com>
2023-01-05 23:38:11 +0200

    local

--- config.py
+++ config.py
@@ -1,13 +1,4 @@
-#Manychat
-manychat_api_url = "https://api.manychat.com"
-manychat_api_key = "Bearer 509996:4acf40aefc22ca693e6763f6b8d425ae"
-manychat_setCustomFields_url = "/fb/subscriber/setCustomFields"
-manychat_sendContentByUserRef = "/fb/sending/sendContentByUserRef"
-manychat_sendFlow = "/fb/sending/sendFlow"
-manychat_headers = {
-      'Authorization': 'Bearer ' + manychat_api_key,
-      'Content-Type': 'application/json'
-    }
+
 
 #tasks
 tasks_quantity = 3
@@ -24,5 +15,42 @@ database = 'loyaltybots'
 db_admintable = 'our_clients'
 db = "db/loyaltybots.db"
 
-#telegram
-token = '5700135657:AAFaZqtAqe2VGBoUi7qhPAlcwCF_W5iMBTk'
+from datetime import datetime
+
+class DBTable:
+    def __init__(self):
+        self.table = None
+        self.cols = None
+
+
+class AdminsTable(DBTable):
+    def __init__(self):
+        super().__init__()
+        self.table = 'our_clients'
+        self.cols = 'id, status, manychat_api, start_date, last_payment_date, end_date, coupons, activated_coupons, points_following, points_comment, points_stories, bot_token, manychat_id, products, template_installed'
+
+
+class AdminCouponsTable(DBTable):
+    def __init__(self, admin_id):
+        super().__init__()
+        self.table = 'coupons_' + str(admin_id)
+        self.cols = 'id, user_id, start_date, end_date, status, activated_date, coupon_name, coupon_cost, bot_username, bot_url'
+
+
+class UsersTable(DBTable):
+    def __init__(self):
+        super().__init__()
+        self.table = 'users'
+        self.cols = 'id, name, telegram_username, bot_startdate, last_message, last_text_input, ref_url, refer, is_admin, points, bonuses, coupons'
+
+
+class Commands:
+    def __init__(self):
+        self.commands_list = ['start', 'coupon_activate', 'settings']
+        self.commands_list_settings = ['change_coupons', 'change_points', 'change_manychat_api']
+        self.commands_list_manual = ['start_manual']
+
+
+current_time = datetime.utcnow()
+
+manychat_ref_url = ''
--- credentials.py
+++ credentials.py
@@ -1,3 +1,13 @@
 bot_token = "5809303804:AAFlJTthvlh0_HFu9ZRAv7g_yQ69eU9tMO8"
 bot_user_name = "loyalty_adminbot"
 webhook_url = f"https://ziabrov.pythonanywhere.com/{bot_token}"
+#Manychat
+manychat_api_url = "https://api.manychat.com"
+manychat_api_key = "Bearer 509996:4acf40aefc22ca693e6763f6b8d425ae"
+manychat_setCustomFields_url = "/fb/subscriber/setCustomFields"
+manychat_sendContentByUserRef = "/fb/sending/sendContentByUserRef"
+manychat_sendFlow = "/fb/sending/sendFlow"
+manychat_headers = {
+      'Authorization': 'Bearer ' + manychat_api_key,
+      'Content-Type': 'application/json'
+    }
